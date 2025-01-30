f72ce6ea53b376a60b76e4111e29a1d7c5523f72
Michele Moio <michelemoiodev@gmail.com>
2025-01-30 08:40:37 +0100

    feat: [RTP] results page and routing

--- src/main.tsx
+++ src/main.tsx
@@ -1,6 +1,5 @@
 import React from 'react';
 import { createRoot } from 'react-dom/client';
-import { App } from './App';
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 import { ApiClient } from './components/ApiClient';
 import { client } from './api/client';
@@ -8,17 +7,25 @@ import { LocalizationProvider } from '@mui/x-date-pickers/LocalizationProvider';
 import { AdapterDayjs } from '@mui/x-date-pickers/AdapterDayjs';
 import 'dayjs/locale/it';
 
-const AppRouter = () => {
-  const auth = useAuth();
-  return <RouterProvider router={router} context={{ auth }} />;
-};
+import { routeTree } from './routeTree.gen';
+import { createRouter, RouterProvider } from '@tanstack/react-router';
+
+const router = createRouter({ routeTree });
+
+declare module '@tanstack/react-router' {
+  interface Register {
+    router: typeof router;
+  }
+}
+
+const queryClient = new QueryClient();
 
 createRoot(document.getElementById('root')!).render(
   <QueryClientProvider client={queryClient}>
     <LocalizationProvider dateAdapter={AdapterDayjs} adapterLocale="it">
       <ApiClient client={client.api} />
       <React.StrictMode>
-        <App />
+        <RouterProvider router={router} />
       </React.StrictMode>
     </LocalizationProvider>
   </QueryClientProvider>,
