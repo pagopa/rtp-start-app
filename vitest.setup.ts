0d2e99e8c3122e50f7274a47444c1912c6829b38
Michele Moio <michelemoiodev@gmail.com>
2025-02-06 16:07:11 +0100

    fix: [typescript] vitest expect extend

--- vitest.setup.ts
+++ vitest.setup.ts
@@ -1,5 +1,7 @@
 import { i18nTestSetup } from './src/__tests__/i18nTestSetup';
-import "vitest-dom/extend-expect";
 import "dayjs/locale/it";
+import * as matchers from "vitest-dom/matchers";
+import { expect } from "vitest";
 
+expect.extend(matchers);
 i18nTestSetup({});
