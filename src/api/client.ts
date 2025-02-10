79b9b4ce5934b9e51842e33d437c3bfc8a6319ae
Michele Moio <michelemoiodev@gmail.com>
2025-02-10 09:06:13 +0100

    chore: [env] add auth url to env (#28)

--- src/api/client.ts
+++ src/api/client.ts
@@ -3,5 +3,5 @@ import { Token } from "../../generated/auth/Token";
 
 export const client = {
   api: new Api({ baseURL: import.meta.env.VITE_API_URL }),
-  auth: new Token({ timeout: 20000 }),
+  auth: new Token({ timeout: 20000, baseURL: import.meta.env.VITE_AUTH_URL }),
 };
