470dcf80b17ad79790f28a23449e42bf6e79315e
gianlucadenunzio <gianluca.denunzio@digitouch.it>
2025-03-12 11:43:14 +0100

    fix: interceptor bug on 401 Unauthorized response (#30)
    Co-authored-by: gdenunzio <gianluca.denunzio@mobilesoft.it>

--- src/pages/KOPage/index.tsx
+++ src/pages/KOPage/index.tsx
@@ -1,16 +1,19 @@
 import { useTranslation } from 'react-i18next';
 import ko from '../../assets/images/ko.svg';
 import { ResultLayout } from 'src/components/ResultLayout';
+import useMessageStore from 'src/stores/message.store';
 
 export default function KOPage() {
   const { t } = useTranslation();
+  const { messageStatus } = useMessageStore() as { messageStatus: "default" | "unauthorized" | undefined };
 
   return (
     <ResultLayout
       image={ko}
-      title={t('KO.title')}
-      body={t('KO.body')}
-      buttonText={t('KO.button')}
+      title={t(`KO.${messageStatus}.title`)}
+      body={t(`KO.${messageStatus}.body`)}
+      buttonText={t(`KO.${messageStatus}.button`)}
+      type={messageStatus}
     />
   );
 }
