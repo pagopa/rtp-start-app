407de6a39504aeb6685cb9793f1af8bcc0e25c0a
gianlucadenunzio <gianluca.denunzio@digitouch.it>
2025-03-24 16:46:25 +0100

    fix: unauthorized redirect

--- src/utils/auth.utils.ts
+++ src/utils/auth.utils.ts
@@ -4,6 +4,7 @@ import useMessageStore from "src/stores/message.store";
 export function invalidateSession() {
   localStorage.removeItem('accessToken');
   localStorage.removeItem('refreshToken');
+  localStorage.removeItem('user');
   useMessageStore.getState().setMessageStatus('unauthorized');
   return router.navigate({to: '/ko'});
 }
